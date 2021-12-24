0cacefcd092cd99765683211c654dea0653231ca
IrmaIsfot <skrappost66@hotmail.com>
2021-12-25 00:46:15 +0100

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,55 @@
+input.onButtonPressed(Button.A, function () {
+    if (Styrkod < 4) {
+        Styrkod = Styrkod + 1
+    } else {
+        Styrkod = 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(Styrkod)
+})
+let Styrkod = 0
+radio.setGroup(1)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+Styrkod = 1
+basic.forever(function () {
+    if (Styrkod == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (Styrkod == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (Styrkod == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
